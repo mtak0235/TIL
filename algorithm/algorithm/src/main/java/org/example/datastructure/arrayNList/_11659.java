489a47574a70a1e40047d4ab904d4bba723b4f71
mtak <jesustark0235@gmail.com>
2023-09-04 00:12:23 +0900

    [알고리즘] 구간합 - 11659 백준

---
+++ algorithm/algorithm/src/main/java/org/example/datastructure/arrayNList/_11659.java
@@ -0,0 +1,28 @@
+package org.example.datastructure.arrayNList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class _11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());//숫자 개수
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());// 질의 개수
+        long[] S = new long[suNo + 1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i - 1]);
+        }
+
+    }
+}
