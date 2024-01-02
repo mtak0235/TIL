01e5b68cf2788d0b4d18a86aab89e9d2cd58f598
mtak <jesustark0235@gmail.com>
2024-01-02 09:02:36 +0900

    1. Hello World

---
+++ java/java-start/src/Comment.java
@@ -0,0 +1,11 @@
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1을 출력한다.
+        //System.out.println("hello java2");
+
+        /* 여러 줄 주석
+        System.out.println("hello java1");
+        System.out.println("hello java1");
+        */
+    }
+}
