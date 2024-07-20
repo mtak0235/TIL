2ea5a4189016b776327ff1feb695f578f48c6220
mtak <jesustark0235@gmail.com>
2024-07-20 22:20:07 +0900

    change architecture

--- spring/mvc/src/main/java/com/mtakworld/mvc/servlet/web/frontcontroller/v3/ControllerV3.java
+++
@@ -1,9 +0,0 @@
-package com.mtakworld.mvc.servlet.web.frontcontroller.v3;
-
-import java.util.Map;
-
-import com.mtakworld.mvc.servlet.web.frontcontroller.ModelView;
-
-public interface ControllerV3 {
-	ModelView process(Map<String, String> map);
-}
