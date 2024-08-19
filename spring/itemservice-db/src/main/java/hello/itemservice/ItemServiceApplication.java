f4b964218d3cf6a4fe97056ee1866fa7849db364
mtak <jesustark0235@gmail.com>
2024-08-20 00:54:08 +0900

    3 testing data access

---
+++ spring/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,41 @@
+package hello.itemservice;
+
+import javax.sql.DataSource;
+
+import hello.itemservice.config.*;
+import hello.itemservice.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+// @Import(MemoryConfig.class)
+@Import(JdbcTemplateV3Config.class)
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+	@Bean
+	@Profile("local")
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
+	}
+
+/*
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setPassword("");
+		dataSource.setUsername("sa");
+		return dataSource;
+	}
+*/
+}
