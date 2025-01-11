dfe6e7c3108753bdcf9ed6b1241cb1484f258fb0
mtak <jesustark0235@gmail.com>
2025-01-11 21:48:29 +0900

    07. synchronized

---
+++ java/java-start/src/thread/sync/BankMain.java
@@ -0,0 +1,23 @@
+package thread.sync;
+
+import static thread.util.MyLogger.*;
+import static thread.util.ThreadUtils.*;
+
+public class BankMain {
+	public static void main(String[] args) throws InterruptedException {
+		BankAccountV3 account = new BankAccountV3(1000);
+		Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
+		Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
+
+		t1.start();
+		t2.start();
+
+		sleep(500);
+		log("t1 state: " + t1.getState());
+		log("t2 state: " + t2.getState());
+
+		t1.join();
+		t2.join();
+		log("최종 잔액: " + account.getBalance());
+	}
+}
